592b0d3e101532198c9fcf0266ae9374de71b04b
mwooddc <mark.wood@dubaicollege.org>
2024-02-27 19:52:06 +0400

    Coaches don't appear in player choices, dropdown arrows added, for a single match entry validation works on choosing singles but selecting two players. Going to work on making this work for multiple match submissions.

--- website/coach.py
+++ website/coach.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, session, flash, redirect, url_for
+from flask import Blueprint, render_template, session, flash, redirect, url_for, request
 from flask_login import login_required, current_user
-from .forms import UserRatingForm, RatingCategoryForm, AddSchoolForm
-from .models import db, User, UserRatings, RatingCategory, School
+from .forms import UserRatingForm, RatingCategoryForm, AddSchoolForm, TennisEventForm, MatchForm
+from .models import db, User, UserRatings, RatingCategory, School, TennisEvent, Match
 from .auth import role_required
+from datetime import datetime
 
 
 #A Blueprint simply allows you to create seperate files from the standard app.py
@@ -89,3 +90,93 @@ def add_school():
         flash('School added successfully!', 'success')
         return redirect(url_for('coach.add_school'))
     return render_template('add_school.html', form=form, user=current_user)
+
+
+
+
+
+
+@coach.route('/create-event-and-matches', methods=['GET', 'POST'])
+@login_required
+@role_required('Coach')
+def create_event_and_matches():
+    event_form = TennisEventForm()
+    match_form = MatchForm()
+
+    if request.method == 'POST':
+        print("Form Data:", request.form)
+        # print("Won or Lost Value:", form_data.get('won_or_lost'))
+
+    # Fetch schools data from the database
+    schools = School.query.all()
+
+    # Populate player choices
+    player_choices = [(str(user.id), f'{user.Forename} {user.Surname}') for user in User.query.filter_by(Role='Player').all()]
+    print("Player Choices:", player_choices)
+
+    match_form.player1_name.choices = [('None', 'None')] + player_choices
+    match_form.player2_name.choices = [('None', 'None')] + player_choices
+
+
+    if request.method == 'POST' and match_form.validate():
+        # Process form data for TennisEvent
+        event_date = datetime.strptime(request.form['date'], '%Y-%m-%d')
+        new_event = TennisEvent(date=event_date,
+                                home_venue_id=request.form['home_venue'],
+                                away_venue_id=request.form['away_venue'])
+        db.session.add(new_event)
+        db.session.commit()
+
+        form_data = dict(request.form)
+        # print("DICTIONARY of form_data",  form_data)
+
+
+        # Initialize a variable to keep track of player index
+        player_index = 0
+
+
+        while True:
+            if player_index == 0:
+                player1_name = form_data["player1_name"]
+                player2_name = form_data["player2_name"]
+                singles_or_doubles = form_data["singles_or_doubles"]
+                sets_played = form_data["sets_played"]
+                sets_won = form_data["sets_won"]
+                won_or_lost = form_data["won_or_lost"]
+                comment = form_data["comment"]
+            else:
+                player_key = f"player1_name_{player_index}"
+                
+                if player_key in form_data:
+                    # Assign values to separate variables
+                    player1_name = form_data.get(f'player1_name_{player_index}', "")
+                    player2_name = form_data.get(f'player2_name_{player_index}', "")
+                    singles_or_doubles = form_data.get(f"singles_or_doubles_{player_index}", "")
+                    sets_played = form_data.get(f"sets_played_{player_index}", "")
+                    sets_won = form_data.get(f"sets_won_{player_index}", "")
+                    won_or_lost = form_data.get(f"won_or_lost_{player_index}", "")
+                    comment = form_data.get(f"comment_{player_index}", "")
+
+                else:
+                    break
+            new_match = Match(tennis_event_id=new_event.id,
+                                        player1_id=player1_name,
+                                        player2_id=player2_name,
+                                        singles_or_doubles=singles_or_doubles,
+                                        sets_played=sets_played,
+                                        sets_won=sets_won,
+                                        won_or_lost=won_or_lost,
+                                        comment=comment)
+            player_index += 1
+            db.session.add(new_match)
+        db.session.commit()
+                    
+            
+
+        return redirect(url_for('coach.create_event_and_matches'))
+    
+    elif request.method == 'POST':
+        # Form validation failed
+        flash('There were errors in your form, please review them.', 'danger')
+
+    return render_template('create_event_and_matches.html', event_form=event_form, match_form=match_form, user=current_user, schools=schools)
--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField
-from wtforms.validators import InputRequired, DataRequired, NumberRange
-from wtforms import SelectField, StringField, SubmitField
+from wtforms.validators import InputRequired, DataRequired, NumberRange, ValidationError
+from wtforms import SelectField, StringField, SubmitField, IntegerField
 from .models import User, RatingCategory
 
 class UserRatingForm(FlaskForm):
@@ -25,3 +24,42 @@ class AddSchoolForm(FlaskForm):
     school_name = StringField('School Name', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
+# class TennisEventForm(FlaskForm):
+#     date = StringField('Date', validators=[InputRequired()])
+#     home_venue = SelectField('Home Venue', coerce=int, validators=[InputRequired()])
+#     away_venue = SelectField('Away Venue', coerce=int, validators=[InputRequired()])
+
+# class MatchForm(FlaskForm):
+#     player1_name = SelectField('Player 1')
+#     player2_name = SelectField('Player 2')
+#     singles_or_doubles = SelectField('Singles or Doubles')
+#     sets_played = IntegerField('Sets Played', validators=[InputRequired()])
+#     sets_won = IntegerField('Sets Won', validators=[InputRequired()])
+#     outcome = SelectField('Outcome')
+#     comment = StringField('Comment')
+
+class TennisEventForm(FlaskForm):
+    date = StringField('Date', validators=[InputRequired()])
+    home_venue = StringField('Home Venue', validators=[InputRequired()])
+    away_venue = StringField('Away Venue', validators=[InputRequired()])
+
+class MatchForm(FlaskForm):
+    player1_name = SelectField('Player 1')
+    # player2_name = SelectField('Player 2')
+    player2_name = SelectField('Player 2', choices=[('', 'None')])  # Assuming you've populated the rest of the choices elsewhere
+    singles_or_doubles = SelectField(
+        'Singles or Doubles',
+        choices=[('Singles', 'Singles'), ('Doubles', 'Doubles')],
+        validators=[DataRequired()]
+    )
+    sets_played = IntegerField('Sets Played', validators=[InputRequired()])
+    sets_won = IntegerField('Sets Won', validators=[InputRequired()])
+    won_or_lost = StringField('won_or_lost')
+    comment = StringField('Comment')
+
+
+    def validate_player2_name(form, field):
+        print("Test",form.singles_or_doubles.data)
+        if form.singles_or_doubles.data == 'Singles' and field.data != 'None':
+            raise ValidationError("For Singles, 'None' must be selected for Player 2.")
